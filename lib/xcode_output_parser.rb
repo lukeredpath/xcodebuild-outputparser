55b7db279e07a9a151b350bf44f766719ed78292
Luke Redpath <contact@lukeredpath.co.uk>
2009-03-02 19:39:36 +0000

    Implemented basic test suite parsing

--- lib/xcode_output_parser.rb
+++ lib/xcode_output_parser.rb
@@ -1,3 +1,86 @@
+require 'time'
+
 module XcodeOutputParser
+  class TestResultParser
+    def parse_output(test_output)
+      start_parsing!
+      test_output.split("\n").each do |line|
+        if line =~ /Test Suite(.*)started/
+          if @current_result.main_test_suite
+            start_test_suite(line)
+          else
+            start_main_test_suite(line)
+          end
+        end
+        if line =~ /Test Suite(.*)finished/
+          if @current_test_suite
+            finish_test_suite(line)
+          else
+            finish_main_test_suite(line)
+          end
+        end
+      end
+      @current_result
+    end
+    
+    private
+    
+    def start_parsing!
+      @current_result = TestResult.new
+      @current_test_suite = nil
+    end
+    
+    def start_main_test_suite(line)
+      suite_name = File.basename(line.match(/'(.*)'/)[1])
+      started_at = Time.parse(line.match(/started at (.*)/)[1])
+      @current_result.main_test_suite = TestResult::TestSuite.new(suite_name)
+      @current_result.main_test_suite.start(started_at)
+    end
+    
+    def finish_main_test_suite(line)
+      finished_at = Time.parse(line.match(/finished at (.*)/)[1])
+      @current_result.main_test_suite.finish(finished_at)
+    end
+    
+    def start_test_suite(line)
+      suite_name = line.match(/'(.*)'/)[1]
+      started_at = Time.parse(line.match(/started at (.*)/)[1])
+      @current_test_suite = TestResult::TestSuite.new(suite_name)
+      @current_test_suite.start(started_at)
+      @current_result.test_suites << @current_test_suite
+    end
+    
+    def finish_test_suite(line)
+      finished_at = Time.parse(line.match(/finished at (.*)/)[1])
+      @current_test_suite.finish(finished_at)
+      @current_test_suite = nil
+    end
+  end
   
+  class TestResult
+    attr_accessor :main_test_suite
+    attr_reader :test_suites
+    
+    def initialize
+      @test_suites = []
+    end
+    
+    class TestSuite
+      attr_reader :name
+      attr_reader :started_at
+      attr_reader :finished_at
+      
+      def initialize(name)
+        @name = name
+      end
+      
+      def start(start_time)
+        @started_at = start_time
+      end
+      
+      def finish(finish_time)
+        @finished_at = finish_time
+      end
+    end
+  end
 end
--- test/xcode_test_output_parser_test.rb
+++ test/xcode_test_output_parser_test.rb
@@ -2,6 +2,87 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w[.. lib]))
+
+require 'xcode_output_parser'
+
 class XcodeTestOutputParserTest < Test::Unit::TestCase
+  
+  context "Parsing a single, empty build product test suite" do
+    setup do
+      @test_output = <<-TESTOUTPUT
+        Test Suite '/Users/luke/Projects/iphone/squeemote/build/Development-iphonesimulator/Testing.app' started at 2009-03-02 19:04:02 +0000
+        Test Suite '/Users/luke/Projects/iphone/squeemote/build/Development-iphonesimulator/Testing.app' finished at 2009-03-02 19:04:02 +0000.
+        Executed 0 tests, with 0 failures (0 unexpected) in 0.00 (0.00) seconds
+      TESTOUTPUT
+      
+      @parser = XcodeOutputParser::TestResultParser.new
+    end
+
+    should "return a TestResult" do
+      assert_instance_of XcodeOutputParser::TestResult, @parser.parse_output(@test_output)
+    end
+    
+    context "and that result" do
+      setup do
+        @result = @parser.parse_output(@test_output)
+      end
+
+      should "contain the main suite name" do
+        assert_equal "Testing.app", @result.main_test_suite.name
+      end
+      
+      should "contain the started at time for the main suite" do
+        assert_equal Time.parse('2009-03-02 19:04:02 +0000'), @result.main_test_suite.started_at
+      end
+      
+      should "contain the finished at time for the main suite" do
+        assert_equal Time.parse('2009-03-02 19:04:02 +0000'), @result.main_test_suite.finished_at
+      end
+    end
+  end
+  
+  context "Result of parsing a single product test suite with one sub-test suite with no test cases" do
+    setup do
+      test_output = <<-TESTOUTPUT
+        Test Suite '/Users/luke/Projects/iphone/squeemote/build/Development-iphonesimulator/Testing.app' started at 2009-03-02 19:04:02 +0000
+        
+        Test Suite 'GTMTestCase' started at 2009-03-02 19:04:02 +0000
+        Test Suite 'GTMTestCase' finished at 2009-03-02 19:04:02 +0000.
+        Executed 0 tests, with 0 failures (0 unexpected) in 0.000 (0.000) seconds
+        
+        Test Suite '/Users/luke/Projects/iphone/squeemote/build/Development-iphonesimulator/Testing.app' finished at 2009-03-02 19:04:02 +0000.
+        Executed 0 tests, with 0 failures (0 unexpected) in 0.00 (0.00) seconds
+      TESTOUTPUT
+
+      @result = XcodeOutputParser::TestResultParser.new.parse_output(test_output)
+    end
+
+    should "contain the main test suite" do
+      assert_not_nil @result.main_test_suite
+    end
+    
+    should "contain the sub test suites" do
+      assert_equal 1, @result.test_suites.length
+    end
+    
+    context "and that sub test suite" do
+      setup do
+        @test_suite = @result.test_suites.first
+      end
+
+      should "have a name" do
+        assert_equal "GTMTestCase", @test_suite.name
+      end
+      
+      should "have a start time" do
+        assert_equal Time.parse('2009-03-02 19:04:02 +0000'), @test_suite.started_at
+      end
+      
+      should "have a finish time" do
+        assert_equal Time.parse('2009-03-02 19:04:02 +0000'), @test_suite.finished_at
+      end
+    end
+  end
   
 end
