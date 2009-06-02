65f6dc3d8333b5992b3f0c869e0c9ca8f3083e0c
Luke Redpath <contact@lukeredpath.co.uk>
2009-06-02 23:06:46 +0100

    Refactored to a delegate-based solution makes it easy to add a new "line-by-line" parsing mode where each line from the build process is parsed in real time.

--- lib/xcode_output_parser.rb
+++ lib/xcode_output_parser.rb
@@ -2,6 +2,15 @@ require 'time'
 
 module XcodeOutputParser
   class TestResultParser
+    attr_reader :delegate
+    attr_reader :previous_line
+    
+    def initialize(delegate = TestResultLineParserDelegate.new)
+      @delegate = delegate
+      @delegate.parser = self
+      @previous_line = nil
+    end
+    
     def self.open(path)
       result = nil
       File.open(path) do |io|
@@ -12,61 +21,78 @@ module XcodeOutputParser
     end
 
     def parse_output(test_output)
-      start_parsing!
+      @delegate.start_parsing!
       test_output.split("\n").each do |line|
-        if line.strip =~ /^Test Suite(.*)started/
-          if @current_result.main_test_suite
-            start_test_suite(line)
-          else
-            start_main_test_suite(line)
-          end
-        end
-        if line.strip =~ /^Test Suite(.*)finished/
-          if @current_test_suite
-            finish_test_suite(line)
-          else
-            finish_main_test_suite(line)
-          end
-        end
-        if line.strip =~ /^Test Case/
-          parse_test_case(line)
-        end
-        @previous_line = line
+        parse_line(line)
       end
-      @current_result
+      @delegate.current_result
     end
     
-    private
+    def parse_output_from_command(cmd)
+      @delegate.start_parsing!
+      IO.popen(cmd) do |io|
+        loop do
+          begin
+            parse_line(io.readline)
+          rescue EOFError
+            break
+          end
+        end
+      end
+      @delegate.finish_parsing!
+    end
     
+    def parse_line(line)
+      if line.strip =~ /^Test Suite(.*)started/
+        @delegate.start_test_suite(line)
+      end
+      if line.strip =~ /^Test Suite(.*)finished/
+        @delegate.finish_test_suite(line)
+      end
+      if line.strip =~ /^Test Case/
+        @delegate.parse_test_case(line)
+      end
+      @previous_line = line
+    end
+  end
+  
+  class TestResultLineParserDelegate
+    attr_reader :current_result
+    attr_accessor :parser
+  
     def start_parsing!
       @current_result = TestResult.new
       @current_test_suite = nil
     end
     
-    def start_main_test_suite(line)
-      suite_name = File.basename(line.match(/'(.*)'/)[1])
-      started_at = Time.parse(line.match(/started at (.*)/)[1])
-      @current_result.main_test_suite = TestResult::TestSuite.new(suite_name)
-      @current_result.main_test_suite.start(started_at)
-    end
-    
-    def finish_main_test_suite(line)
-      finished_at = Time.parse(line.match(/finished at (.*)/)[1])
-      @current_result.main_test_suite.finish(finished_at)
+    def finish_parsing!
+      return @current_test_suite
     end
     
     def start_test_suite(line)
-      suite_name = line.match(/'(.*)'/)[1]
-      started_at = Time.parse(line.match(/started at (.*)/)[1])
-      @current_test_suite = TestResult::TestSuite.new(suite_name)
-      @current_test_suite.start(started_at)
-      @current_result.test_suites << @current_test_suite
+      if @current_result.main_test_suite
+        suite_name = line.match(/'(.*)'/)[1]
+        started_at = Time.parse(line.match(/started at (.*)/)[1])
+        @current_test_suite = TestResult::TestSuite.new(suite_name)
+        @current_test_suite.start(started_at)
+        @current_result.test_suites << @current_test_suite
+      else
+        suite_name = File.basename(line.match(/'(.*)'/)[1])
+        started_at = Time.parse(line.match(/started at (.*)/)[1])
+        @current_result.main_test_suite = TestResult::TestSuite.new(suite_name)
+        @current_result.main_test_suite.start(started_at)
+      end
     end
     
     def finish_test_suite(line)
-      finished_at = Time.parse(line.match(/finished at (.*)/)[1])
-      @current_test_suite.finish(finished_at)
-      @current_test_suite = nil
+      if @current_test_suite
+        finished_at = Time.parse(line.match(/finished at (.*)/)[1])
+        @current_test_suite.finish(finished_at)
+        @current_test_suite = nil
+      else
+        finished_at = Time.parse(line.match(/finished at (.*)/)[1])
+        @current_result.main_test_suite.finish(finished_at)
+      end
     end
     
     def parse_test_case(line)
@@ -75,7 +101,7 @@ module XcodeOutputParser
         test_case = TestResult::TestCase.new(test_name, true)
       elsif line.match(/failed/)
         test_case = TestResult::TestCase.new(test_name, false)
-        test_case.failure_message = @previous_line.strip
+        test_case.failure_message = @parser.previous_line.strip
       end
       @current_test_suite.test_cases << test_case
     end
